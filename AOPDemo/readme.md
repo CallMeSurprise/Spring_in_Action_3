e8fbdf4ea43ad3992ab582bd65ed45c15e3d853e
think <shijingqi2011@163.com>
2015-10-22 08:54:42 +0800

    重新测试通过的Demo

--- AOPDemo/readme.md
+++
@@ -1,1 +0,0 @@
-### this is a simple demo for implemention of Spring AOP.
--- AOPDemo/src/main/java/com/lf/App.java
+++ AOPDemo/src/main/java/com/lf/App.java
@@ -1,12 +1,14 @@
 package com.lf;
 
-//run this java file as Java Application
-
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App {
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
     public static void main(String[] args) {
         ApplicationContext ctx=new ClassPathXmlApplicationContext("demo.xml");
         Performer p=(Performer) ctx.getBean("duck");
